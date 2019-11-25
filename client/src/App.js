beb9af56edbdd19a6c01762cf44ae8212c17480c
Marcos Cortorreal <marcos.cortorreal@marcos.cortorreal-mbp>
2019-11-25 18:02:14 -0500

    too many changes to name, but added a catch to server index.js file for pgclient to exit with 1 if it doesn't calculate values

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,25 @@
-import React from 'react';
-import './App.css';
-import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
-import Fib from './Fib';
-import OtherPage from './OtherPage';
+import React from "react";
+import "./App.css";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import Fib from "./Fib";
+import OtherPage from "./OtherPage";
 
 function App() {
-	return (
-		<Router>
-			<div className='App'>
-				<header className='App-header'>
-					<Link to='/'>Home</Link>
-					<Link to='/otherpage'>Other Page</Link>
-					<div>
-						<Route exact path='/' component={Fib} />
-						<Route path='/otherpage' component={OtherPage} />
-					</div>
-				</header>
-			</div>
-		</Router>
-	);
+  return (
+    <Router>
+      <div className="App">
+        <header className="App-header">
+          <h1 className="App-title">Fib Calculator</h1>
+          <Link to="/">Home</Link>
+          <Link to="/otherpage">Other Page</Link>
+          <div>
+            <Route exact path="/" component={Fib} />
+            <Route path="/otherpage" component={OtherPage} />
+          </div>
+        </header>
+      </div>
+    </Router>
+  );
 }
 
 export default App;
